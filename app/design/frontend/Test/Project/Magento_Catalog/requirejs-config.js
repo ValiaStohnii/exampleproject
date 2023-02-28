23f99c6b77be0a3edc394249dbacc9786f0934ca
valia <v.stohnii@elogic.com.ua>
2023-02-28 17:08:00 +0200

    add attribute

---
+++ app/design/frontend/Test/Project/Magento_Catalog/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    map: {
+        "*": {
+            descriptionTable: "Magento_Catalog/js/description-table",
+            askForMoreSubmit: "Magento_Catalog/js/ask-for-more-submit",
+            productSlickCarousel: "Magento_Catalog/js/product-slick-carousel",
+        },
+    },
+};
