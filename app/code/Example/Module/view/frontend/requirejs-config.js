fe4b50e644f4e025933f995839a7621b01132b92
valia <v.stohnii@elogic.com.ua>
2023-03-14 17:02:08 +0200

    Create RequireJS aliases with map and paths

--- app/code/Example/Module/view/frontend/requirejs-config.js
+++ app/code/Example/Module/view/frontend/requirejs-config.js
@@ -1,4 +1,12 @@
 var config ={
-
+    "map": {
+        "*": {
+            "fadeInElement": "Example_Module/js/fade-in-element"
+        }
+    },
+    "paths": {
+        "vue": "https://unpkg.com/vue@3.2.47/dist/vue.global"
+        // "vue": "Example_Module/js/vue"
+    }
 }
 console.log('see you')
