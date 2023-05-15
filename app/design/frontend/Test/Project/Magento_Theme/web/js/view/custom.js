15a138c9e44d9b19f4571ffaad6714ecb54d537b
valia <v.stohnii@elogic.com.ua>
2023-05-15 14:44:24 +0300

    UI translated

--- app/design/frontend/Test/Project/Magento_Theme/web/js/view/custom.js
+++ app/design/frontend/Test/Project/Magento_Theme/web/js/view/custom.js
@@ -1,9 +1,10 @@
-define(['uiComponent', 'ko'], (uiComponent, ko)=>{
+define(['uiComponent', 'ko', mage/transalte], (uiComponent, ko, $t)=>{
     'use strict'
 
     return uiComponent.extend({
         defaults: {
-            text: ko.observable('default text')
+            text: ko.observable('default text'),
+            text2: $t('default text')
         },
 
         initialize(){
