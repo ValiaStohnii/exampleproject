8f149d1aedc7546585fc22dd16fa9fa68fefaa6e
valia <v.stohnii@elogic.com.ua>
2023-06-12 16:53:50 +0300

    checkout

--- app/code/Internship/ProductQuestion/view/frontend/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config ={
-    "map": {
-        "*": {
-            "productQuestionComponent": "Internship_ProductQuestion/js/product-question-component"
-        }
-    }
-}
-console.log('see you')
--- app/design/frontend/Test/Project/Magento_Catalog/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    map: {
-        "*": {
-            descriptionTable: "Magento_Catalog/js/description-table",
-            askForMoreSubmit: "Magento_Catalog/js/ask-for-more-submit",
-            productSlickCarousel: "Magento_Catalog/js/product-slick-carousel",
-        },
-    },
-};
